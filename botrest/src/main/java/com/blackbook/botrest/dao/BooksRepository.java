8c814ff4a05a1c48038ca8fbf715db584a951242
Mateusz Slabonski <mateusz_slabonski@epam.com>
2017-08-21 18:13:27 +0200

    Books Controller Integration Tests

--- botrest/src/main/java/com/blackbook/botrest/dao/BooksRepository.java
+++ botrest/src/main/java/com/blackbook/botrest/dao/BooksRepository.java
@@ -6,16 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 /**
  * @author Siarhei Shauchenka
  * @since 16.08.17
  */
 public interface BooksRepository extends CrudRepository<Book, Long> {
-
-    List<Book> findByPriceGreaterThan(double price);
-    List<Book> findByPriceLessThan(double price);
 
     @Query("SELECT b FROM Book b WHERE b.author LIKE %?1% OR b.title LIKE %?1%")
     Page<Book> findBooksWithTextualSearch(String query, Pageable pageable);
