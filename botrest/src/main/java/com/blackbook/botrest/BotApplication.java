9f216e8897e20ab0f89b12971be1573cad26ed51
patrycja_z <klimczak.patrycja@gmail.com>
2017-08-21 16:48:17 +0200

    CORS enabled for all origins and GET,POST,PUT,DELETE methods

--- botrest/src/main/java/com/blackbook/botrest/BotApplication.java
+++ botrest/src/main/java/com/blackbook/botrest/BotApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BotApplication {
@@ -15,7 +18,19 @@ public class BotApplication {
         servletRegistrationBean.addUrlMappings("/console/*");
         return servletRegistrationBean;
     }
-  
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
+
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
     }
