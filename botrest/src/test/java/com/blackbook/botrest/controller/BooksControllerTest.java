0ed308622263174ec1c873a5f504f10aa841c238
patrycja_z <klimczak.patrycja@gmail.com>
2017-08-22 19:08:39 +0200

    Book post test refactored to check json body

--- botrest/src/test/java/com/blackbook/botrest/controller/BooksControllerTest.java
+++ botrest/src/test/java/com/blackbook/botrest/controller/BooksControllerTest.java
@@ -2,19 +2,29 @@ package com.blackbook.botrest.controller;
 
 import com.blackbook.botrest.dao.BooksRepository;
 import com.blackbook.botrest.model.Book;
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.path.json.config.JsonPathConfig;
 import org.apache.http.HttpStatus;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.JsonConfig.jsonConfig;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
 import static org.testng.AssertJUnit.assertEquals;
 
 /**
@@ -46,30 +56,44 @@ public class BooksControllerTest {
                 .when()
                 .get(BOOKS_API)
                 .then()
+                .body("content.author[1]", equalTo("Tymek Robert"))
                 .statusCode(HttpStatus.SC_OK);
     }
 
     @Test
     public void postBookExpectCreatedBook() {
 
+        String author = "Tymoteusz";
+        String title = "Nero";
+        Double price = 65.99;
+
         Book book = new Book();
-        book.setAuthor("Tymoteusz");
-        book.setPrice(65.99);
-        book.setTitle("Nero");
+        book.setAuthor(author);
+        book.setTitle(title);
+        book.setPrice(price);
 
         given()
                 .port(port)
                 .contentType(ContentType.JSON)
+                .config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.DOUBLE)))
                 .body(book)
                 .when()
                 .post(BOOKS_API)
                 .then()
+                .body("author", equalTo(author))
+                .body("title", equalTo(title))
+                .body("price", is(price))
                 .statusCode(HttpStatus.SC_OK);
 
-        Book givenBook = booksRepository.findOne(5L);
-        book.setId(5);
+        Pageable pageable = mock(Pageable.class);
+        Page<Book> returnedBook = booksRepository.findBooksWithTextualSearch(title, pageable);
+        Book bookFromRepo = returnedBook.getContent()
+                .stream()
+                .findFirst()
+                .get();
 
-        assertEquals(book, givenBook);
+        book.setId(bookFromRepo.getId());
+        assertEquals(book, bookFromRepo);
     }
 
 }
