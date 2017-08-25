72a585231bda40b18844c6f9f7fc6edc95d48d6e
Sergey <Siarhei_Schauchenka@epam.com>
2017-08-25 12:47:55 +0200

    start to test scheduler part (in progress...)

---
+++ scheduler/src/test/java/com/blackbook/scheduler/controller/SchedulerRestControllerTest.java
@@ -0,0 +1,8 @@
+package com.blackbook.scheduler.controller;
+
+/**
+ * @author Siarhei Shauchenka
+ * @since 25.08.17
+ */
+public class SchedulerRestControllerTest {
+}
---
+++ scheduler/src/test/resources/books_controller_test_dummy_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO observers(url) VALUES('some url1');
+INSERT INTO observers(url) VALUES('some url2');
+INSERT INTO observers(url) VALUES('some url3');
+INSERT INTO observers(url) VALUES('some url4');
