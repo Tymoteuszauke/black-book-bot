1943e4fcb79c1aec04c572f5ef53171521a79321
patkkka <klimczak.patrycja@gmail.com>
2017-08-25 11:33:14 +0200

    Revert "Bus feature"

--- scheduler/src/main/java/com/blackbook/scheduler/processor/core/RequestProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.blackbook.scheduler.processor.core;
-
-/**
- * @author Siarhei Shauchenka
- * @since 23.08.17
- */
-public interface RequestProcessor extends Runnable {
-    int OK_STATUS = 200;
-}
--- scheduler/src/main/resources/migration/V1_01__initial.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE observers (
-    id int not null auto_increment primary key,
-    url varchar(300),
-);
