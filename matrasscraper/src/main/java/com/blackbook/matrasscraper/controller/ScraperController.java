c395d17c42eaeac2e9fc59eba58adbc83487a1c7
Patrycja Zaremba <klimczak.patrycja@gmail.com>
2017-08-31 00:00:15 +0200

    Awful scraper start - just to play around with jsoup

--- matrasscraper/src/main/java/com/blackbook/matrasscraper/controller/ScraperController.java
+++ matrasscraper/src/main/java/com/blackbook/matrasscraper/controller/ScraperController.java
@@ -1,10 +1,13 @@
 package com.blackbook.matrasscraper.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author "Patrycja Zaremba"
  */
 @RestController
+@RequestMapping(value = "/api/matrasscrapper")
 public class ScraperController {
+
 }
