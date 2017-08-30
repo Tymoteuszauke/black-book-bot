a1f1b9ec02ff6b53824a1953982f8ed6c5dcf4e5
patrycja_z <klimczak.patrycja@gmail.com>
2017-08-30 10:49:18 +0200

    New module added for Matras scrapper

---
+++ matrasscraper/src/main/java/com/blackbook/matrasscraper/controller/ScraperController.java
@@ -0,0 +1,10 @@
+package com.blackbook.matrasscraper.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author "Patrycja Zaremba"
+ */
+@RestController
+public class ScraperController {
+}
