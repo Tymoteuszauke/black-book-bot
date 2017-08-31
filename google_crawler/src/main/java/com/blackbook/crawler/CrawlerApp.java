8be986a3906bccf359e0549260e4bf512266b616
Piotr Sekula <piotr92345@o2.pl>
2017-08-31 14:37:03 +0200

    Add swagger to restbot, crawler and persistancebot modules; fix readme

--- google_crawler/src/main/java/com/blackbook/crawler/CrawlerApp.java
+++ google_crawler/src/main/java/com/blackbook/crawler/CrawlerApp.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  *
@@ -18,6 +22,15 @@ public class CrawlerApp {
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new WebServlet());
         servletRegistrationBean.addUrlMappings("/console/*");
         return servletRegistrationBean;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     public static void main(String[] args) {
--- persistencebot/src/main/java/com/blackbook/persistencebot/PersistenceApplication.java
+++ persistencebot/src/main/java/com/blackbook/persistencebot/PersistenceApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class PersistenceApplication {
@@ -14,6 +18,15 @@ public class PersistenceApplication {
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new WebServlet());
         servletRegistrationBean.addUrlMappings("/console/*");
         return servletRegistrationBean;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     public static void main(String[] args) {
