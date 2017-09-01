a18efd02a3e6307c372bb08d477918bf12af7e70
Piotr Sekula <piotr92345@o2.pl>
2017-09-01 17:35:55 +0200

    Add reading necessary book info from html file

--- czytampl_scraper/src/main/java/com/blackbook/czytamplscraper/controller/ScraperController.java
+++ czytampl_scraper/src/main/java/com/blackbook/czytamplscraper/controller/ScraperController.java
@@ -1,12 +1,7 @@
-package com.blackbook.matrasscraper.controller;
+package com.blackbook.czytamplscraper.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.blackbook.matrasscraper.htmlprovider.JsoupHTMLDocumentProvider;
-import com.blackbook.matrasscraper.scraper.Scraper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -18,12 +13,9 @@ import view.book_discount.BookDiscountView;
 
 import java.util.List;
 
-/**
- * @author "Patrycja Zaremba"
- */
 @Slf4j
 @RestController
-@RequestMapping(value = "/api/matras-scraper")
+@RequestMapping(value = "/api/czytampl-scraper")
 public class ScraperController {
 
     @Value("${endpoints.persistence-api}")
@@ -31,13 +23,14 @@ public class ScraperController {
 
     @RequestMapping(method = RequestMethod.POST)
     public List<BookDiscountView> postBookDiscounts() {
-        log.info("Transaction: POST /api/matras-scraper");
+        //log.info("Transaction: POST /api/czytampl-scraper");
         ClientHttpRequestFactory requestFactory = new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()); //getClientHttpRequestFactory();
         RestTemplate restTemplate = new RestTemplate(requestFactory);
 
-        Scraper scraper = new Scraper(new JsoupHTMLDocumentProvider());
-        HttpEntity<Object> request = new HttpEntity<>(scraper.extractBookElements());
+//        Scraper scraper = new Scraper(new JsoupHTMLDocumentProvider());
+//        HttpEntity<Object> request = new HttpEntity<>(scraper.extractBookElements());
 
-        return (List<BookDiscountView>) restTemplate.postForObject(persistenceApiEndpoint + "/api/book-discounts", request, List.class);
+//        return (List<BookDiscountView>) restTemplate.postForObject(persistenceApiEndpoint + "/api/book-discounts", request, List.class);
+        return null;
     }
 }
--- czytampl_scraper/src/main/java/com/blackbook/czytamplscraper/scraper/Scraper.java
+++ czytampl_scraper/src/main/java/com/blackbook/czytamplscraper/scraper/Scraper.java
@@ -1,7 +1,5 @@
 package com.blackbook.czytamplscraper.scraper;
 
-import com.blackbook.czytamplscraper.scraper.Preparer;
-import com.blackbook.czytamplscraper.scraper.WebReader;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -12,6 +10,7 @@ import java.util.List;
 public class Scraper {
 
     private static final String startPage = "http://czytam.pl/tania-ksiazka.html";
+    private final static int BOOKSTORE_ID = 2;
 
 //    Elements elements = doc.select(".product");
 
@@ -24,7 +23,7 @@ public class Scraper {
         String promotionUrl = promotionPages.get(5);
         Document promotionPage = reader.getDocumentFromWebPage(promotionUrl);
         Elements elements = promotionPage.select(".product");
-        Element book = elements.get(15);
+        Element book = elements.get(22);
         System.out.println(book.html());
         System.out.println("\n-------------------------------------------\n");
 
@@ -37,5 +36,19 @@ public class Scraper {
         List<String> prices = book.select(".product-price").select("strong").eachText();
         System.out.println("Old price: " + prices.get(0));
         System.out.println("New price: " + prices.get(1));
+
+        Elements promoDetails = book.select(".image-container").select("a[href]");
+        System.out.println("Promo details: " + promoDetails.text().replaceAll("\\s+", ""));
+        System.out.println("Cover url: " + promoDetails.select("img").attr("src"));
+
+        String bookUrl = "http://czytam.pl" + book.select(".image-container").select("a").attr("href");
+        System.out.println("Book url: " + bookUrl);
+
+//        Document detailsPage = reader.getDocumentFromWebPage(bookUrl);
+        //System.out.println(detailsPage.html());
+
+//        Elements subtitle = detailsPage.getElementById("panel4-2").child(2).select("strong");
+//        System.out.println("Subtitle: " + subtitle.html());
+
     }
 }
