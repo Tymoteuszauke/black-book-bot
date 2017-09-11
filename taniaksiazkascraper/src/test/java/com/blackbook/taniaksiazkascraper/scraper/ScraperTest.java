2654625177a70152905324c4cab5db0926514e82
Piotr Sekula <piotr92345@o2.pl>
2017-09-11 12:44:38 +0200

    Refactor scraper test to check more than one page

--- taniaksiazkascraper/src/test/java/com/blackbook/taniaksiazkascraper/scraper/ScraperTest.java
+++ taniaksiazkascraper/src/test/java/com/blackbook/taniaksiazkascraper/scraper/ScraperTest.java
@@ -20,18 +20,24 @@ public class ScraperTest {
     private Connector mockConnector;
     private LastPageChecker mockChecker;
     private Document document;
+    private Document lastDocument;
     private final String TEST_URL = "http://www.taniaksiazka.pl/tanie-ksiazki/page-1";
+    private final String TEST_LAST_URL = "http://www.taniaksiazka.pl/tanie-ksiazki/page-2";
     private final File PROMOTIONS_HTML_FILE = new File("src/test/resources/promotions_page.html");
+    private final File PROMOTIONS_LAST_PAGE_HTML_FILE = new File("src/test/resources/promotions_last_page.html");
 
     @BeforeTest
     public void before() throws IOException {
         document = Jsoup.parse(PROMOTIONS_HTML_FILE, "UTF-8");
+        lastDocument = Jsoup.parse(PROMOTIONS_LAST_PAGE_HTML_FILE, "UTF-8");
 
         mockConnector = mock(Connector.class);
         when(mockConnector.getDocumentFromWebPage(TEST_URL)).thenReturn(document);
+        when(mockConnector.getDocumentFromWebPage(TEST_LAST_URL)).thenReturn(lastDocument);
 
         mockChecker = mock(LastPageChecker.class);
-        when(mockChecker.isLastPage(document)).thenReturn(true);
+        when(mockChecker.isLastPage(document)).thenReturn(false);
+        when(mockChecker.isLastPage(lastDocument)).thenReturn(true);
 
         scraper = new Scraper(mockConnector, mockChecker, new PromoDetailsReader());
     }
@@ -42,6 +48,6 @@ public class ScraperTest {
         List<BookDiscountData> bookDiscountData = scraper.extractBookElements();
 
         // Then
-        Assert.assertEquals(28, bookDiscountData.size());
+        Assert.assertEquals(56, bookDiscountData.size());
     }
 }
