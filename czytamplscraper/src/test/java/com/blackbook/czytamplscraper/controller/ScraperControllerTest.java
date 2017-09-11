087799162e98cfd171e57288efee783a66ff6b01
Piotr Sekula <piotr92345@o2.pl>
2017-09-11 09:32:40 +0200

    Add return status to czytam.pl scraper controller

--- czytamplscraper/src/test/java/com/blackbook/czytamplscraper/controller/ScraperControllerTest.java
+++ czytamplscraper/src/test/java/com/blackbook/czytamplscraper/controller/ScraperControllerTest.java
@@ -3,12 +3,14 @@ package com.blackbook.czytamplscraper.controller;
 import com.blackbook.czytamplscraper.service.ScraperService;
 import com.jayway.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import view.response.SimpleResponse;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.mockito.Mockito.doNothing;
@@ -42,5 +44,18 @@ public class ScraperControllerTest {
 
         // Then
         verify(scraperService, times(1)).saveResultsInDatabase();
+    }
+
+    @Test
+    public void shouldReturnProperResponse() {
+        // Given
+        doNothing().when(scraperService).saveResultsInDatabase();
+        ScraperController controller = new ScraperController(scraperService);
+
+        // When
+        SimpleResponse getResponse = controller.postBookDiscounts();
+
+        // Then
+        Assert.assertEquals("Czytam.pl scraper results saved in database!", getResponse.getMessage());
     }
 }
