c5a5e0cafa51534f72798ba1cfb62a102383543d
patrycja_z <klimczak.patrycja@gmail.com>
2017-09-12 16:16:45 +0200

    Main method added to Models

---
+++ models/src/main/java/MainClass.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author "Patrycja Zaremba"
+ */
+@SpringBootApplication
+public class MainClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class);
+    }
+}
